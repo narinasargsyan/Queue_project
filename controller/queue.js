212dc04906c2fea86a90a0c2f173e03e272f6b9f
Vlad Sargsyan <volodya.sargsyan@all.me>
2022-08-06 15:08:28 +0400

    return all queues for user

--- controller/queue.js
+++ controller/queue.js
@@ -1,22 +1,22 @@
-const models = require('../db/models'); 
- 
- class QueueController { 
- 
-   add = async (req,res) => { 
+const models = require('../db/models');
+
+ class QueueController {
+
+   add = async (req,res) => {
        const { id }  = req.verifedUser;
 
-       await models.Queues.create({ 
+       await models.Queues.create({
           userId: id,
           status: 0,
        });
-       res.send('done'); 
+       res.send('done');
     };
 
    getClient = async (req,res) => {
         const { id } = req.body;
-        const queue = await models.Queues.findOne({ 
-         where: { id } 
-        }); 
+        const queue = await models.Queues.findOne({
+         where: { id }
+        });
 
         if(queue.status !== 0) {
             return res.status(406).send('Queue already in processing');
@@ -28,24 +28,24 @@ const models = require('../db/models');
         res.send('Get Client');
     };
 
-   
+
    queueFinished = async (req,res) => {
-      try { 
+      try {
          const { id } = req.body;
-         const queue = await models.Queues.findOne({ 
-            where: { id } 
-         }); 
+         const queue = await models.Queues.findOne({
+            where: { id }
+         });
 
          if(queue.status !== 1) {
             return res.status(406).send('Queue shoul be in processing to finish');
-         }  
+         }
 
          if(queue.workerId === req.verifedUser.id) {
             await models.Queues.update( { status: 2 }, {
                where: { id }
             });
             return res.send(200)
-         } 
+         }
          res.status(406).send('Invalid Queue Id');
       } catch(e) {
          console.log('eeee', e)
@@ -55,18 +55,15 @@ const models = require('../db/models');
 
    getOnOwn = async (req,res) => {
     try {
-      const { id, userId, workerId, status } = req.body;
-         const queueAll = await models.Queues.findAll({ 
-            where: { 
-              id,
-              userId,
-              workerId,
-              status,
-            } 
-         }); 
+         const queueAll = await models.Queues.findAll({
+            where: {
+              userId: req.verifedUser.id,
+            }
+         });
+        res.status(200).json(queueAll);
     } catch (e) {
-      console.log('eeee', e)
-         res.send('something went wrong')
+        console.log('Error', e)
+        res.send('something went wrong')
     }
    };
 
